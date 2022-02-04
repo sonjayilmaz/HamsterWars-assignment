475576639cd3bf0c953ed463bd752b280209743d
sonjayilmaz <sonja_yilmaz@hotmail.com>
2022-02-04 13:24:30 +0100

    Post and get single hamster added

--- backend/routes/hamsters.js
+++ backend/routes/hamsters.js
@@ -20,10 +20,37 @@ router.get('/', async (req, res) =>{
         hamsters.push( hamster )
     })
     res.send(hamsters)
-})
+});
+
+
 
 //get /hamsters/:id
+router.get('/:id', async(req, res) => {
+    const id = req.params.id
+    const hamstersRef = await db.collection('hamsters').doc(id).get()
+    if( !hamstersRef.exists ) {
+        res.status(404).send('Hamster does not exist')
+        return
+    }
+    const data = hamstersRef.data()
+    res.send(data)
+})
+
 //post /hamsters
+router.post('/', async(req, res) => {
+    const object = req.body
+
+    if( !object.name || typeof object.age != 'number' || !object.favFood || !object.loves || !object.imgName || 
+    typeof object.wins != 'number' || typeof object.defeats != 'number' || typeof object.games != 'number' ) {
+        res.sendStatus(400) 
+        return
+    }
+        const hamstersRef = await db.collection('hamsters').add(object)
+        res.send(hamstersRef.id)
+    })
+
+
+
 //put /hamsters/:id
 //delete /hamsters/:id
 
